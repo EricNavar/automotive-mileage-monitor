f6be6817b750b14da5daa071e627dc51bdbc83d2
Eric Navar <eric.navar@asurion.com>
2022-10-27 02:06:10 -0400

    change to light theme

--- frontend-professional/src/dummyData.js
+++ frontend-professional/src/dummyData.js
@@ -16,5 +16,77 @@ export const dummyResults = [
         "make": "Acura",
         "model": "TL Type-S",
         "year": "2008"
+    },
+    {
+        "yearlyFuelCost": 1855.25,
+        "originalMileage": "25",
+        "effectiveMileage": 20.66,
+        "speed": 30,
+        "make": "Acura",
+        "model": "TSX",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 1572.49,
+        "originalMileage": "26",
+        "effectiveMileage": 24.375,
+        "speed": 40,
+        "make": "Acura",
+        "model": "Integra Type-R",
+        "year": "2001"
+    },
+    {
+        "yearlyFuelCost": 1484.55,
+        "originalMileage": "26",
+        "effectiveMileage": 25.819,
+        "speed": 50,
+        "make": "Acura",
+        "model": "ZDX",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 2412.33,
+        "originalMileage": "16",
+        "effectiveMileage": 15.889,
+        "speed": 60,
+        "make": "Aston Martin",
+        "model": "V8 Vantage",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 2555.3,
+        "originalMileage": "16",
+        "effectiveMileage": 15,
+        "speed": 70,
+        "make": "Aston Martin",
+        "model": "V8 Vantage",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 3092.09,
+        "originalMileage": "15",
+        "effectiveMileage": 12.396,
+        "speed": 80,
+        "make": "Aston Martin",
+        "model": "Virage",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 3873.23,
+        "originalMileage": "15",
+        "effectiveMileage": 9.896,
+        "speed": 90,
+        "make": "Aston Martin",
+        "model": "Virage",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 3369.63,
+        "originalMileage": "26",
+        "effectiveMileage": 11.375,
+        "speed": 100,
+        "make": "Audi",
+        "model": "RS 4",
+        "year": "2008"
     }
 ];
