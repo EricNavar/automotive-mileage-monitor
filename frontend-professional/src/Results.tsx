48b57b67d41a632471b69051901e01ffd47ac463
Eric Navar <eric.navar@asurion.com>
2022-10-27 00:56:30 -0400

    add results screen

--- frontend-professional/src/Results.tsx
+++ frontend-professional/src/Results.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { dummyResults } from './dummyData';
+import './results.css';
+import Saul from './assets/saul.png';
+
+type ListItemProps = {
+    yearlyFuelCost: number;
+    originalMileage: string;
+    effectiveMileage: number;
+    speed: number;
+    make: string;
+    model: string;
+    year: string;
+}
+
+const ListItem = (props: ListItemProps) => {
+    return (
+        <div className="listItem">
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <div style={{backgroundImage:`url(${Saul})`}} className="car-thumbnail"/>
+            <div className="info-container">
+                <p className="line1">{props.year} {props.make} {props.model}</p>
+                <p className="line2">
+                    <span className="gray-text">MPG:</span> <span className="mileage">{props.originalMileage}</span> → <span className='real-mileage'>{props.effectiveMileage}</span><span style={{marginLeft:"2rem"}}>
+                    <span className="gray-text">Speed: </span>{props.speed} mph</span>
+                </p>
+                <p className="line3"><span className="gray-text">Yearly fueld cost: </span>${props.yearlyFuelCost}</p>
+            </div>
+        </div>
+    );
+};
 
 const Results = () => {
-    return (
-        <div>
+    const meanOriginalMileage = dummyResults.reduce((total, next) => total + Number(next.originalMileage), 0) / dummyResults.length;
+    const meanEffectiveMileage = dummyResults.reduce((total, next) => total + next.effectiveMileage, 0) / dummyResults.length;
+    const meanSpeed = dummyResults.reduce((total, next) => total + next.speed, 0) / dummyResults.length;
+    let meanYearlyFuelCost = dummyResults.reduce((total, next) => total + next.yearlyFuelCost, 0) / dummyResults.length;
+    meanYearlyFuelCost = Math.round(meanYearlyFuelCost * 100) / 100; //round to the nearest cent
 
+    return (
+        <div className="results">
+            <h1>Results</h1>
+            <div className="list-container">
+                <div className="list">
+                    {dummyResults.map((item, index) => <ListItem key={index} {...item}/>)}
+                </div>
+            </div>
+            <div className="total" style={{padding: 8, borderRadius: 8}}>
+                <p style={{marginBottom:8, marginTop: 0}}><b>Averages</b></p>
+                <p><span className="gray-text">Mileage: </span><span className="mileage">{meanOriginalMileage}</span> → <span className='real-mileage'>{meanEffectiveMileage}</span></p>
+                <p><span className="gray-text">Speed: </span>{meanSpeed} mph</p>
+                <p><span className="gray-text">Yearly Fuel Cost: </span>${meanYearlyFuelCost}</p>
+            </div>
         </div>
     );
 }
---
+++ frontend-professional/src/dummyData.js
@@ -0,0 +1,20 @@
+export const dummyResults = [
+    {
+        "yearlyFuelCost": 3369.63,
+        "originalMileage": "26",
+        "effectiveMileage": 11.375,
+        "speed": 10,
+        "make": "Acura",
+        "model": "TL",
+        "year": "2012"
+    },
+    {
+        "yearlyFuelCost": 2234.57,
+        "originalMileage": "26",
+        "effectiveMileage": 17.153,
+        "speed": 20,
+        "make": "Acura",
+        "model": "TL Type-S",
+        "year": "2008"
+    }
+];
